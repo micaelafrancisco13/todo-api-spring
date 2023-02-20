83a4de858d62fd26447ce8fd80dcd4fe19495a34
Micaela Francisco <micaela.francisco.s@bulsu.edu.ph>
2023-02-20 13:51:59 +0800

    changed CORS access origin

--- src/main/java/com/hooli/todo/controllers/CategoryController.java
+++ src/main/java/com/hooli/todo/controllers/CategoryController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class CategoryController {
     private final CategoryService categoryService;
 
--- src/main/java/com/hooli/todo/controllers/NoteController.java
+++ src/main/java/com/hooli/todo/controllers/NoteController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/notes")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class NoteController {
     private final NoteService noteService;
 
